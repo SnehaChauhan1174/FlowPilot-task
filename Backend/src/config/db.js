5d3a7c362b7aa70b66e759aebc424f6cddc1a840
sneha <snehachauhan5002@gmail.com>
2025-12-14 16:39:52 +0530

    final changes in .env path

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -1,4 +1,7 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config({ path: "../.env" });
+
 const connectDB=async()=>{
     try{
         await mongoose.connect(process.env.MONGO_URL);
