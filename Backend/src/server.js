c96827ffe63fb2bfa3e21b77cdd6455ec27d5cfb
sneha <snehachauhan5002@gmail.com>
2025-12-14 12:45:14 +0530

    Server file modified

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,11 +1,23 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+
+const app = express();
+// const port = 3000
+
+app.use(express.json());
+app.use(cors());
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+const port=process.env.PORT;
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
